3b4997500f3ea704ed9eb19e06eec867d091e8e8
DatBoi12-01101 <daniellittle211@gmail.com>
2024-07-17 18:26:24 -0700

    Change cell shading to allow  for multiple light type being detected

--- Project Strongest/Assets/FGCMaker/URP_Assets/Shaders/Cel_Shading/CelShading.hlsl
+++ Project Strongest/Assets/FGCMaker/URP_Assets/Shaders/Cel_Shading/CelShading.hlsl
@@ -1,3 +1,4 @@
+
 void CelShading_float(in float3 Normal, in float Smoothness, in float3 ClipSpacePos, in float3 WorldPos, in float4 tinting, in float rampOffset, out float3 rampOutput, out float3 direction)
 {
 #ifdef SHADERGRAPH_PREVIEW
@@ -27,27 +28,4 @@ void CelShading_float(in float3 Normal, in float Smoothness, in float3 ClipSpace
 	direction = light.direction;
 #endif
 }
-void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, 
-	float3 WorldView, float MainDiffuse, float MainSpecular, float3 MainColor, 
-	out float Diffuse, out float Specular, out float3 Color) 
-{
-	Diffuse = MainDiffuse;
-	Specular = MainSpecular;
-	Color = MainColor * (MainDiffuse + MainSpecular);
-#ifndef SHADERGRAPH_PREVIEW
-	int pixelLightCount = GetAdditionalLightsCount();
-	for (int i = 0; i < pixelLightCount; i++) 
-	{
-		Light addLight = GetAdditionalLight(i, WorldPosition);
-		half NdotL = saturate(dot(WorldNormal, addLight.direction));
-		half atten = addLight.distanceAttenuation * addLight.shadowAttenuation;
-		half thisDiffuse = atten * NdotL;
-		half thisSpecular = LightingSpecular(thisDiffuse, addLight.direction, WorldNormal, WorldView, 1, Smoothness);
-		Diffuse += thisDiffuse;
-		Specular += thisSpecular;
-		Color += addLight.color * (thisDiffuse + thisSpecular);
-	}
-#endif
-	half total = Diffuse + Specular;
-	Color = total <= 0 ? MainColor : Color / total;
-}
+
