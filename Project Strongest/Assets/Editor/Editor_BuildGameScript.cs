5ad90f2f93d6e7979d92ffffa1ba7439a8710d60
DatBoi12-01101 <daniellittle211@gmail.com>
2024-10-14 21:39:39 -0700

    Change to setup build script for game

---
+++ Project Strongest/Assets/Editor/Editor_BuildGameScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+#if UNITY_EDITOR
+public class Editor_BuildGameScript : MonoBehaviour
+{
+}
+#endif
