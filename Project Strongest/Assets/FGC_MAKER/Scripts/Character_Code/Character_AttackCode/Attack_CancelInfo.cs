e5e8b35595429989e0b04a473fe943065a412856
DatBoi12-01101 <daniellittle211@gmail.com>
2024-11-12 19:40:06 -0800

    Change to ordering and addition of noncancelable command normals

--- Project Strongest/Assets/FGC_MAKER/Scripts/Character_Code/Character_AttackCode/Attack_CancelInfo.cs
+++ Project Strongest/Assets/FGC_MAKER/Scripts/Character_Code/Character_AttackCode/Attack_CancelInfo.cs
@@ -5,7 +5,6 @@ using System;
 [Serializable]
 public class Attack_CancelInfo 
 {
-    //public Cancel_State cancelFrom;
     public Cancel_State CurrentLevel;
     public Cancel_State nextAvailableAttackRoute;
 }
@@ -13,20 +12,21 @@ public class Attack_CancelInfo
 public enum Cancel_State 
 {
     NotCancellable = 1 << 0,
-    Light_Normal_Attack = 1 << 1,
-    Heavy_Normal_Attack = 1 << 2,
-    Command_Normal_Attack = 1 << 3,
-    Light_String_Normal_Start = 1 << 4,
-    Light_String_Normal_FollowUp = 1 << 5,
-    Heavy_String_Normal_Start = 1 << 6,
-    Heavy_String_Normal_FollowUp = 1 << 7,
-    Special_Attack = 1 << 8,
-    Rekka_Input_Start = 1 << 9,
-    Stance_Input_Start = 1 << 10,
-    Rekka_Input_FollowUp = 1 << 11,
-    Stance_Input_FollowUp = 1 << 12,
-    Super_Attack = 1 << 13,
-    Maximum_Attack = 1 << 14,
-    Throw = 1 << 15,
-    Parry = 1 << 16,
+    Parry = 1 << 1,
+    Throw = 1 << 2,
+    Light_Normal_Attack = 1 << 3,
+    Heavy_Normal_Attack = 1 << 4,
+    Command_Normal_Attack_Cancelable = 1 << 5,
+    Command_Normal_Attack_Noncancelable = 1 << 6,
+    Light_String_Normal_Start = 1 << 7,
+    Light_String_Normal_FollowUp = 1 << 8,
+    Heavy_String_Normal_Start = 1 << 9,
+    Heavy_String_Normal_FollowUp = 1 << 10,
+    Special_Attack = 1 << 11,
+    Rekka_Input_Start = 1 << 12,
+    Stance_Input_Start = 1 << 13,
+    Rekka_Input_FollowUp = 1 << 14,
+    Stance_Input_FollowUp = 1 << 15,
+    Super_Attack = 1 << 16,
+    Maximum_Attack = 1 << 17,
 }
