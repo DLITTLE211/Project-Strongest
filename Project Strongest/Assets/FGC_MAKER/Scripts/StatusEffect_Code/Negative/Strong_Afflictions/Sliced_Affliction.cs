921d7525602980bbd1a629bb7a60b1c58c2d5a7e
DatBoi12-01101 <daniellittle211@gmail.com>
2024-12-11 19:01:18 -0800

    Start of affliction script management for afflictions

--- Project Strongest/Assets/FGC_MAKER/Scripts/StatusEffect_Code/Negative/Strong_Afflictions/Sliced_Affliction.cs
+++ Project Strongest/Assets/FGC_MAKER/Scripts/StatusEffect_Code/Negative/Strong_Afflictions/Sliced_Affliction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FleetFood_Amplify : Amplifiers
+public class Sliced_Affliction : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
