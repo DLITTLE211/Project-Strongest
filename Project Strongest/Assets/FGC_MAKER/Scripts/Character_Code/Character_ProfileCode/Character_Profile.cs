7da05b252a719cca13d50ceb68cc4162ded51dc2
DatBoi12-01101 <daniellittle211@gmail.com>
2025-03-24 22:13:16 -0700

    Change to include all remaining art assets for project alpha state

--- Project Strongest/Assets/FGC_MAKER/Scripts/Character_Code/Character_ProfileCode/Character_Profile.cs
+++ Project Strongest/Assets/FGC_MAKER/Scripts/Character_Code/Character_ProfileCode/Character_Profile.cs
@@ -14,6 +14,7 @@ public class Character_Profile : ScriptableObject
     public string CharacterName;
     public int CharacterID;
     public Sprite CharacterProfileImage;
+    public Sprite CharacterSelectIcon;
     #endregion
 
     #region Character Sizing Info
